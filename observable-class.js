fb9b5e2cde34388e28342ec90c8d4d0d49fbe108
PhuChau <vinhphu101195@gmail.com>
2019-05-23 16:22:20 +0300

    forEach and Map observable

---
+++ observable-class.js
@@ -0,0 +1,51 @@
+function Observable(forEach) {
+  this._forEach = forEach;
+}
+
+Observable.prototype = {
+  forEach: function(onNext, onError, onCompleted) {
+    if (typeof onNext === "function") {
+      return this._forEach({
+        onNext: onNext,
+        onError: onError || function() {},
+        onCompleted: onCompleted || function() {}
+      });
+    } else {
+      return this._forEach(onNext);
+    }
+  },
+
+  map: function(projectionFunction) {
+    var self = this;
+    //mapped observable
+    return new Observable(function forEach(Observer) {
+      // self here is forEach above
+      return self.forEach(
+        function onNext(x) {
+          observer.onNext(projectionFunction(x));
+        },
+        function onError(e) {
+          observer.onError(e);
+        },
+        function onCompleted() {
+          observer.onCompleted();
+        }
+      );
+    });
+  }
+};
+
+Observable.fromEvent = function(dom, eventName) {
+  return new Observable(function forEach(observer) {
+    var handler = e => observer.onNext(e);
+
+    dom.addEventListener(eventName, handler);
+
+    //Subscription
+    return {
+      dispose: () => {
+        dom.removeEventListener(eventName, handler);
+      }
+    };
+  });
+};
